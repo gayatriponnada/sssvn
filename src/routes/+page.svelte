582053a1d759b57b6f6d20497e04643dfef286bc
gayatriponnada <gayatriponnada160@gmail.com>
2024-05-03 15:54:22 +0530

    new update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,19 @@
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
 	import Notifications from '$lib/components/notifications.svelte';
-	import School from '$lib/images/school.jpg';
-	import Chairman from '$lib/images/chairman.jpeg';
-	import Director from '$lib/images/director.jpeg';
+	import Homepageimage1 from '$lib/images/home-page-image1.png';
+	import Homepageimage2 from '$lib/images/home-page-image2.jpg';
+
 	const images = [
 		{
 			id: 1,
-			src: School,
+			src: Homepageimage1,
 			alt: 'School'
 		},
 		{
 			id: 2,
-			src: Chairman,
-			alt: 'Chairman'
-		},
-		{
-			id: 3,
-			src: Director,
-			alt: 'Director'
+			src: Homepageimage2,
+			alt: 'School'
 		}
 	];
 	const plugin = Autoplay({ delay: 2000, stopOnInteraction: true });
@@ -29,27 +24,28 @@
 <svelte:head>
 	<title>Sri Shiridi Sai Vidya Niketan</title>
 </svelte:head>
-
-<Carousel.Root
-	plugins={[plugin]}
-	class="w-full max-w-xs"
-	on:mousenter={plugin.stop}
-	on:mouseleave={plugin.reset}
->
-	<Carousel.Content>
-		{#each images as image (image.id)}
-			<Carousel.Item>
-				<div class="p-1">
-					<Card.Root>
-						<Card.Content class="flex aspect-square items-center justify-center p-6">
-							<img src={image.src} alt="hey" />
-						</Card.Content>
-					</Card.Root>
-				</div>
-			</Carousel.Item>
-		{/each}
-	</Carousel.Content>
-	<Carousel.Previous />
-	<Carousel.Next />
-</Carousel.Root>
+<div class="flex justify-center">
+	<Carousel.Root
+		plugins={[plugin]}
+		class="w-full m-auto max-w-xs"
+		on:mousenter={plugin.stop}
+		on:mouseleave={plugin.reset}
+	>
+		<Carousel.Content class="">
+			{#each images as image (image.id)}
+				<Carousel.Item>
+					<div class="p-1">
+						<Card.Root>
+							<Card.Content class="flex aspect-square items-center justify-center p-6">
+								<img src={image.src} alt="hey" class="h-4/5 w-4/5" />
+							</Card.Content>
+						</Card.Root>
+					</div>
+				</Carousel.Item>
+			{/each}
+		</Carousel.Content>
+		<Carousel.Previous />
+		<Carousel.Next />
+	</Carousel.Root>
+</div>
 <Notifications />
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,4 +1,37 @@
 <script lang="ts">
+	import Events1 from '$lib/images/events1.jpeg';
+	import Events2 from '$lib/images/events2.jpeg';
+	import Events3 from '$lib/images/events3.jpeg';
+	import Events4 from '$lib/images/events4.jpg';
+	import Events5 from '$lib/images/events5.jpeg';
+	import Events6 from '$lib/images/events6.jpg';
 </script>
 
-<h1>events</h1>
+<h2 class="text-primary text-3xl font-extralight pl-10 pt-10">Events</h2>
+
+<div class="flex justify-evenly">
+	<div class=" flex items-center w-1/4 h-1/4">
+		<img src={Events1} alt="Events1" />
+		<div class="w-1/4 h-1/4">
+			<h4 class="text-2xl text-primary py-5">Kreedosthav</h4>
+			<p class="text-justify">
+				On the 22nd of August 2024, the Sports Day unfolded with an extra layer of excitement and
+				anticipation as the distinguished Chess player Koneru Humpy, graced the event as the chief
+				guest.
+			</p>
+			<!--The atmosphere crackled with enthusiasm as
+				students, athletes, and fans eagerly awaited her presence. With her inspiring presence, the
+				sports field transformed into a stage for not just competition but also for celebration of
+				athleticism and sportsmanship. Serena's motivational words echoed across the field, igniting
+				the spirits of all present. As athletes showcased their skills and determination, Serena's
+				presence served as a beacon of inspiration, reminding everyone of the values of hard work,
+				perseverance, and fair play. Her participation added a touch of prestige and honor to the
+				event, leaving an indelible mark on the memories of all who attended. -->
+		</div>
+	</div>
+</div>
+<!-- <img src={Events2} alt="Events2" class="" />
+<img src={Events3} alt="Events3" class="" />
+<img src={Events4} alt="Events4" class="" />
+<img src={Events5} alt="Events5" class="" />
+<img src={Events6} alt="Events6" class="" /> -->
